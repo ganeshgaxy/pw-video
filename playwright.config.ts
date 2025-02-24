f19184e2769bf0d777e94e6953cd0fbc0c9e72b8
Ganeshan Manickam <ganeshan.manickam@showpad.com>
2025-02-24 11:04:16 +0100

    Added initial project

---
+++ playwright.config.ts
@@ -0,0 +1,46 @@
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+  // Look for test files in the "tests" directory, relative to this configuration file.
+  testDir: "specs",
+
+  // Run all tests in parallel.
+  fullyParallel: true,
+
+  // Fail the build on CI if you accidentally left test.only in the source code.
+  forbidOnly: !!process.env.CI,
+
+  // Retry on CI only.
+  retries: process.env.CI ? 1 : 0,
+
+  // Opt out of parallel tests on CI.
+  workers: process.env.CI ? 1 : undefined,
+
+  // Reporter to use
+  reporter: "html",
+
+  use: {
+    // Base URL to use in actions like `await page.goto('/')`.
+    baseURL: "http://127.0.0.1:3000",
+
+    // Collect trace when retrying the failed test.
+    trace: "on-first-retry",
+  },
+  // Configure projects for major browsers.
+  projects: [
+    {
+      name: "chromium",
+      use: {
+        ...devices["Desktop Chrome"],
+        launchOptions: {
+          args: [
+            "--no-sandbox",
+            "--disable-setuid-sandbox",
+            "--use-fake-device-for-media-stream",
+            "--use-fake-ui-for-media-stream",
+          ],
+        },
+      },
+    },
+  ],
+});
---
+++ specs/sample.spec.ts
@@ -0,0 +1,19 @@
+import test, { expect } from "@playwright/test";
+
+test.describe("Sample Video Suite", () => {
+  test("Sample Video Test", async ({ page }) => {
+    await page.goto("https://webcamtests.com/check");
+    await page.locator(".fc-cta-consent").click({ timeout: 10000 });
+    await page.locator("button#webcam-launcher").click({ timeout: 10000 });
+    await page.waitForTimeout(5000);
+    await page
+      .locator("button[data-action='stopWebcam']")
+      .click({ timeout: 10000 });
+    const text = await page
+      .locator("li.notice-injected.notice-info")
+      .innerText();
+    const secondsString = text.split("The duration of video playback:")[1];
+    const seconds = parseFloat(secondsString.split("s")[0]);
+    expect(seconds).toBeGreaterThan(0);
+  });
+});
