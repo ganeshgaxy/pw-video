128c3bc6c969682c215e84abb47197a74b3c7fea
Ganeshan Manickam <ganeshan.manickam@showpad.com>
2025-02-24 11:12:26 +0100

    Adjust for consent button

--- specs/sample.spec.ts
+++ specs/sample.spec.ts
@@ -3,7 +3,9 @@ import test, { expect } from "@playwright/test";
 test.describe("Sample Video Suite", () => {
   test("Sample Video Test", async ({ page }) => {
     await page.goto("https://webcamtests.com/check");
-    await page.locator(".fc-cta-consent").click({ timeout: 10000 });
+    if (await page.locator(".fc-cta-consent").isVisible({ timeout: 10000 })) {
+      await page.locator(".fc-cta-consent").click();
+    }
     await page.locator("button#webcam-launcher").click({ timeout: 10000 });
     await page.waitForTimeout(5000);
     await page
